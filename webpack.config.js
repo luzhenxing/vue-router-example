ded01811f44257415c2ab98cae60f046a788299b
luzhenxing <luzhenxing_1230@163.com>
2016-12-16 18:27:23 +0800

    多文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const webpack = require('webpack'),
   path = require('path'),
+  glob = require('glob'),
   CleanPlugin = require('clean-webpack-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
-  OpenBrowserPlugin = require('open-browser-webpack-plugin');
+  OpenBrowserPlugin = require('open-browser-webpack-plugin'),
 
-module.exports = {
-  entry: ['./src/main.js'],
+  entrys = {},
+  entrys_arr = glob.sync(path.join(__dirname, 'src/entry/*.js'));
+
+const getEntry = entryName => {
+  const entry = entryName.replace(/.*\/entry\/(\w+)\.js/, '$1');
+  entrys[entry] = entryName;
+};
+
+entrys_arr.forEach(getEntry);
+
+const config = {
+  entry: entrys,
   output: {
     publicPath: '/dist/',
     path: './dist',
@@ -23,16 +34,30 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': '../node_modules/vue/dist/vue.common.js'
+      'vue$': path.join(__dirname, 'node_modules/vue/dist/vue.common.js')
     }
   },
   plugins: [
     new CleanPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-      template: path.join(__dirname, 'index.html'),
-      inject: 'body'
-    }),
+    // 提供公共代码
+    new webpack.optimize.CommonsChunkPlugin('common.js'), // 默认会把所有入口节点的公共代码提取出来,生成一个common.js
+    // new HtmlWebpackPlugin({
+    //   filename: './index.html',
+    //   template: path.join(__dirname, 'index.html'),
+    //   inject: 'body'
+    // }),
     // new OpenBrowserPlugin({ url: 'http://localhost:8080/dist/index.html' })
   ]
 }
+
+for (let entry in entrys) {
+  console.log(entry);
+  config.plugins.push(new HtmlWebpackPlugin({
+    filename: `${entry}.html`,
+    template: path.join(__dirname, 'src/tpl/', `${entry}.html`),
+    chunks: ['common.js', entry],
+    inject: true
+  }));
+}
+
+module.exports = config;
